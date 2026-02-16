dad3ea05b3d5b47a1dcdf21ad27779fc7eef2e22
a62177919-commits <a62177919@gmail.com>
2026-02-16 17:25:04 +0400

    Update main.py

--- main.py
+++ main.py
@@ -2,14 +2,22 @@
 # API_HASH: 44f1cdcc4c6544d60fe06be1b319d2dd
 
 import os
+import sys
 from telethon import TelegramClient, events
 from telethon.sessions import StringSession
 from telethon.tl.functions.photos import UploadProfilePhotoRequest
 from telethon.tl.functions.account import UpdateProfileRequest
 
+# Данные
 API_ID = 34126767
 API_HASH = "44f1cdcc4c6544d60fe06be1b319d2dd"
+
+# Берем из секретов GitHub (через YAML)
 SESSION_STR = os.environ.get("SESSION_STR")
+
+if not SESSION_STR:
+    print("❌ ОШИБКА: STRING_SESSION не передан в код!")
+    sys.exit(1)
 
 client = TelegramClient(StringSession(SESSION_STR), API_ID, API_HASH)
 
@@ -29,17 +37,12 @@ async def change_name(event):
     await client(UpdateProfileRequest(first_name=new_name))
     await event.delete()
 
-@client.on(events.NewMessage(pattern=r'\.setbio (.+)', outgoing=True))
-async def change_bio(event):
-    new_bio = event.pattern_match.group(1)
-    await client(UpdateProfileRequest(about=new_bio))
-    await event.delete()
-
 # ----КАТЕГОРИЯ: СИСТЕМА----
 @client.on(events.NewMessage(pattern=r'\.ping', outgoing=True))
 async def ping(event):
     await event.edit("OK")
 
 if __name__ == "__main__":
+    print("🚀 Festka Bot запущен...")
     client.start()
     client.run_until_disconnected()
